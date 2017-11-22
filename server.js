1129d63e76bf41852cfb306c262a4a62a7006026
davzer <david.xox@gmail.com>
2017-11-22 10:54:30 +0100

    https

--- server.js
+++ server.js
@@ -16,3 +16,15 @@ app.get('/aboutus', function(req, res) { res.render("about.ejs"); });
 app.get('/contactus', mailController.sendEmail);
 
 app.listen(process.env.PORT || 8080);
+
+app.enable('trust proxy');
+
+app.use (function (req, res, next) {
+        if (req.secure) {
+                // request was via https, so do no special handling
+                next();
+        } else {
+                // request was via http, so redirect to https
+                res.redirect('https://' + req.headers.host + req.url);
+        }
+});
