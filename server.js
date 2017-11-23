7d87559b41bbf5978c33bef3ba43aec3acc129c2
davzer <david.xox@gmail.com>
2017-11-23 11:08:22 +0100

    up for order

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mailController = require('./controllers/mailController.js');
 var app = express();
+
 app.enable('trust proxy');
 app.use (function (req, res, next) {
         if (req.secure) {
@@ -23,6 +24,7 @@ app.get('/', function(req, res) { res.render("index.ejs"); });
 app.get('/grille', function(req, res) { res.render("grille.ejs"); });
 app.get('/cgv', function(req, res) { res.render("cgv.ejs"); });
 app.get('/aboutus', function(req, res) { res.render("about.ejs"); });
+app.get('/commander', function(req, res) { res.render("commander.ejs"); });
 app.get('/contactus', mailController.sendEmail);
 
 app.listen(8080);
